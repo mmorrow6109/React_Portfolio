febe297ef7a49d26896b2ec2268c06afeff3e95c
mmorrow6109 <msmorrow6109@gmail.com>
2024-03-28 15:19:12 -0700

    nav styling updated.  downloaded react icons

--- React_Portfolio/src/components/nav.jsx
+++ React_Portfolio/src/components/nav.jsx
@@ -7,11 +7,11 @@ const Nav = () => {
                 <div>
                     <a href="#" className="text-white font-mono text-3x1 tracking-wider flex items-center">PORTFOLIO</a>
                 </div>
-                <div>
-                    <a href="#">About</a>
-                    <a href="#">Skills</a>
-                    <a href="#">Projects</a>
-                    <a href="#">Contact</a>
+                <div className="space-x-4">
+                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">About</a>
+                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Skills</a>
+                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Projects</a>
+                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Contact</a>
                 </div>
             </div>
         </>
