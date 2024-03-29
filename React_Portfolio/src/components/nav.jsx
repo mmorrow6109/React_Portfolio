609f188a0e0ca9ebeefaa80383212ea8bd8de0c3
mmorrow6109 <msmorrow6109@gmail.com>
2024-03-28 17:13:48 -0700

    need to fix toggle () but the inital nav and responsiveness should be set

--- React_Portfolio/src/components/nav.jsx
+++ React_Portfolio/src/components/nav.jsx
@@ -1,21 +1,67 @@
-
+import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
+import { AiOutlineClose } from "react-icons/ai";
+import { hiMenuAlt1 } from "react-icons/hi";
+import { useState } from "react";
 
 const Nav = () => {
+    const [toggle, setToggle] = useState(false);
+
+    function openMenu() { 
+        setToggle(true);
+    }
+
+    function closeMenu() {
+        setToggle(false);
+    }
+
     return (
         <>
             <div className="flex item-center justify-between p-10 lg:flex-row text-black-600">
                 <div>
-                    <a href="#" className="text-white font-mono text-3x1 tracking-wider flex items-center">PORTFOLIO</a>
+                    <a href="#" className="text-white font-mono text-3xl tracking-wider flex items-center">
+                        <FaBeer style={{ marginRight: '6px' }} />
+                        PORTFOLIO
+                    </a>
                 </div>
                 <div className="space-x-4">
-                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">About</a>
-                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Skills</a>
-                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Projects</a>
-                    <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Contact</a>
+                    <div className="hidden md:block lg:block xl:block space-x-2 sm:hidden">
+                        <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">About</a>
+                        <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Skills</a>
+                        <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Projects</a>
+                        <a href="#" className="text-white hover:bg-indigo-800 rounded-full px-5 py-2 text-xl">Contact</a>
+                    </div>
+                    <div className="block md:hidden">
+                        {toggle ? (
+                            <AiOutlineClose onClick={closeMenu} size={30} className="text-white cursor-pointer" />
+                        ) : (
+                            <hiMenuAlt1 onClick={openMenu} size={30} className="text-white cursor-pointer" />
+                        )}
+                    </div>
                 </div>
             </div>
+
+            <div className="block md:hidden">
+                {toggle ? (
+                    <div className="flex justify-between ml-10">
+                        <ul>
+                            <li>
+                                <a href="#" className="text-white text-xl mb-2 cursor-pointer">About</a>
+                            </li>
+                            <li>
+                                <a href="#" className="text-white text-xl mb-2 cursor-pointer">Skills</a>
+                            </li>
+                            <li>
+                                <a href="#" className="text-white text-xl mb-2 cursor-pointer">Projects</a>
+                            </li>
+                            <li>
+                                <a href="#" className="text-white text-xl mb-2 cursor-pointer">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                ) : null}
+            </div>
         </>
-    )
+    );
 };
 
 export default Nav;
--- React_Portfolio/tailwind.config.js
+++ React_Portfolio/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: { // Tailwind CSS breakpoints
+     ssm: "375px",
+     sm: "640px",
+     md: "768px",
+     lg: "1024px",
+     xl: "1280px",
+    },
     extend: {},
   },
   plugins: [],
