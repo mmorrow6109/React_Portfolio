6e27acf5b6bc69c77b476ee2f58ae4a59f0a246c
mmorrow6109 <msmorrow6109@gmail.com>
2024-04-01 12:41:46 -0700

    reformatted padding and margins a bit

--- React_Portfolio/src/components/hero.jsx
+++ React_Portfolio/src/components/hero.jsx
@@ -1,42 +1,46 @@
 import {BsGithub, BsLinkedin} from 'react-icons/bs';
 import HeroPic from '../assets/heroPic.jpg'
 
-const hero = () => {
+const Hero = () => {
     return (
-        <section className='flex justify-around items-center p-10 space-x-10 lg:flex-row ssm:flex-col ssm:space-y-10 text-white'>
-            <div className='lg:w-1/3 ssm:w-auto'>
-                <p className='text-3xl mb-5 text-slate-300'>I'm</p>
-                    <h1 className='text-5xl'>Matt Morrow</h1>
-                    <hr className="mt-3 border-t-2 border-gray-400"/>
-                <p className='mt-10 text-l text-slate-300 font-sans'>
+        <section className='flex justify-around items-center p-5 lg:p-10 space-y-5 lg:space-y-10 lg:flex-row ssm:flex-col ssm:space-y-5 text-white'>
+            {/* Left Section */}
+            <div className='lg:w-1/4 ssm:w-1/3 ssm:pr-5'>
+                <p className='text-3xl mb-2 lg:mb-5 text-slate-300'>I'm</p>
+                <h1 className='text-5xl mb-2 lg:mb-3'>Matt Morrow</h1>
+                <hr className="mt-2 lg:mt-3 border-t-2 border-gray-400"/>
+                <p className='mt-2 lg:mt-3 text-lg lg:text-xl text-slate-300 font-sans'>
                     Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
                 </p>
             </div>
 
-            <div className='w-1/3 items-center ssm:w-auto'>
-                <img src={HeroPic} alt='Matt Morrow, Architect' className='max-w-full h-auto border-8 border-white rounded-full' style={{ width: '100%', maxWidth: '300px' }}/>
+            {/* Middle Section */}
+            <div className='w-1/3 items-center ssm:w-1/2'>
+                <div className="flex justify-center"> {/* Center the image */}
+                    <img src={HeroPic} alt='Matt Morrow, Architect' className='max-w-full h-auto border-8 border-white rounded-full' style={{ maxWidth: '300px' }}/>
+                </div>
             </div>
 
-            <div className='w-1/3 ssm:w-auto'>
-                <p className='text-4xl mb-4'>About Me</p>
-                <p className='text-slate-300'>
+            {/* Right Section */}
+            <div className='lg:w-1/4 ssm:w-1/3 ssm:pl-5 lg:pl-0'>
+                <p className='text-4xl mb-2 lg:mb-3'>About Me</p>
+                <p className='text-slate-300 mb-4 lg:mb-5'>
                     Let's build something amazing together.
                 </p>
-                <button className='bg-white text-indigo-600 px-10 py-2 my-3 rounded-full group relative'>
+                <button className='bg-white text-indigo-600 px-8 lg:px-10 py-2 my-3 rounded-full group relative'>
                     <span className="inline-block">
                         Show More...
                     </span>
                     <span className="absolute inset-0 rounded-full border-4 border-transparent hover:border-indigo-800"></span>
                 </button>
 
-                <div className='flex mt-5 space-x-4 cursor-pointer'>
+                <div className='flex space-x-4 cursor-pointer'>
                     <BsGithub size={40} className='group rounded-full hover:border-4 hover:border-indigo-800'/>
                     <BsLinkedin size={40} className='group rounded-full hover:border-4 hover:border-indigo-800'/>
                 </div>
             </div>
         </section>
-
     );
 };
 
-export default hero;
+export default Hero;
