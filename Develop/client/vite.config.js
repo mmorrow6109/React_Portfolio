6f560922cea95f01d67b71749e594556c918e4a3
mmorrow6109 <msmorrow6109@gmail.com>
2024-04-24 17:11:10 -0700

    vite.config updated

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -7,7 +7,13 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-  }
-})
-
-
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        '/src/main.jsx',
+        // add any other external modules here if needed
+      ],
+    },
+  },
+});
