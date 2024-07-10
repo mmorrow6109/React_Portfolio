8a5a607fe7b47ec04ddf81a644307c6dd29adaad
mmorrow6109 <msmorrow6109@gmail.com>
2024-07-10 14:38:43 -0700

    server commented out. sendEmail.js updated

--- Develop/functions/sendEmail.js
+++ Develop/functions/sendEmail.js
@@ -4,32 +4,32 @@ require('dotenv').config();
 exports.handler = async function(event, context) {
     const { name, email, message } = JSON.parse(event.body);
 
-    // Nodemailer transporter setup
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: process.env.EMAIL,
-            pass: process.env.PASSWORD
-        }
+            pass: process.env.PASSWORD,
+        },
     });
 
     const mailOptions = {
         from: email,
         to: process.env.EMAIL,
         subject: `Contact Form Submission from ${name}`,
-        text: message
+        text: message,
     };
 
     try {
         await transporter.sendMail(mailOptions);
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: 'Message sent successfully!' })
+            body: JSON.stringify({ message: 'Message sent successfully!' }),
         };
     } catch (error) {
+        console.error('Error sending email:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: 'Failed to send message. Please try again later.', error: error.toString() })
+            body: JSON.stringify({ message: 'Failed to send message. Please try again later.', error: error.toString() }),
         };
     }
 };
